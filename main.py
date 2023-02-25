beb7bf22114cf0021cd41d7ea892d4f25f7b2807
Santiago Montero <monterosantiago2001@gmail.com>
2023-02-25 20:15:45 -0300

    Feat : add the events to the bot

--- main.py
+++ main.py
@@ -2,17 +2,43 @@
 import os
 
 import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
+from discord.ext import commands
 
 client = discord.Client(intents=discord.Intents.default())
+TOKEN = 'MTA3OTE2MDcwMzI4MzM2MzkwMg.GgI1qc.nWweg7V9N8ZIwpeSyZkm3Jfj02fFE8yf6nOP_k'
+GUILD = '1008842568831877221'
+
+
+bot = commands.Bot(command_prefix='_', description="this is a testing bot")
+
+
+# Ping-pong
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+        print(
+            f'{client.user} is connected to the following guild:\n' f'{guild.name}(id: {guild.id})')
+
+
+@client.event
+async def on_message(message):
+    if 'hola' in message.content.lower():
+        print(message)
+        await message.channel.send('hola ' + message.author.name)
+
+
+@client.command
+async def on_message(message):
+    if 'hola' in message.content.lower():
+        print(message)
+        await message.channel.send('hola ' + message.author.name)
+
 
 client.run(TOKEN)
